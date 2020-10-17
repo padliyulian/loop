4601affc93f15eec75901f5398a519a7fca3e894
padli yulian <padliyulian@gmail.com>
2020-10-17 21:59:51 +0700

    adding factory & seeder

---
+++ database/factories/PostFactoy.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Models\Post;
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker) {
+    $title = $faker->sentence();
+    return [
+        'user_id' => $faker->numberBetween($min = 1, $max = 3),
+        'title' => $title,
+        'slug' => str_replace(' ', '-', strtolower($title)),
+        'content' => "<p>".$faker->paragraph()."</p>"
+    ];
+});
